ea8cf12feb5a9827dd539b64ea14da4c734f1dac
Jürg Messmer <jmessmer@bluewin.ch>
2011-01-07 11:39:41 +0100

    file added?

---
+++ modules/widget/subscriber_group_input/SubscriberGroupInputWidget.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* @package modules.widget
+*/
+class SubscriberGroupInputWidgetModule extends WidgetModule {
+	
+	public function getSubscriberGroups() {
+		$bGetGroupsWithSubscribersOnly = false;
+		$aSubscriberGroups = SubscriberGroupPeer::getSubscriberGroups(true, $bGetGroupsWithSubscribersOnly);
+		$aSubscriberGroups = WidgetJsonFileModule::jsonBaseObjects($aSubscriberGroups, array('id', 'name'));
+		// TODO: handle external mail groups
+		// FilterModule::getFilters()->handleExternalMailGroups(array('mail_groups' => &$aSubscriberGroups));
+		return $aSubscriberGroups;
+	}
+}
