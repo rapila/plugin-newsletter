a50a2a40c7c65cf39b9701d63d1945dfcb6863d7
Jürg Messmer <jmessmer@bluewin.ch>
2011-06-18 17:59:38 +0200

    sort of versandgruppe fixed by joining newsletter_mailings

--- modules/widget/newsletter_list/NewsletterListWidgetModule.php
+++ modules/widget/newsletter_list/NewsletterListWidgetModule.php
@@ -104,4 +104,11 @@ class NewsletterListWidgetModule extends WidgetModule {
 	public function getLanguageName() {
 		return StringPeer::getString('language.'.$this->oDelegateProxy->getLanguageId(), null, $this->oDelegateProxy->getLanguageId());
 	}
+	
+  public function getCriteria() {
+		$oCriteria = new Criteria();
+    $oCriteria->addJoin(NewsletterPeer::ID, NewsletterMailingPeer::NEWSLETTER_ID, Criteria::LEFT_JOIN);
+		return $oCriteria;
+	}
+
 }
