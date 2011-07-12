246d6cfb6ada16e0599a95d4ccd69b87bcc4d41b
Raphael Schweikert <any@sabberworm.com>
2011-07-12 11:10:42 +0200

    added comment

--- lib/classes/NewsletterMailer.php
+++ lib/classes/NewsletterMailer.php
@@ -27,6 +27,7 @@ class NewsletterMailer {
 			// unsubscribe page is required, a page that contains a content object NewsletterFrontendModule, ie the subscribe page
 			$this->oUnsubscribePage = PagePeer::getPageByIdentifier(Settings::getSetting('newsletter_plugin', 'unsubscribe_page', 'subscribe'));
 			if ($this->oUnsubscribePage === null) {
+				// Fallback: try searching the page by name
 				$this->oUnsubscribePage = PagePeer::getPageByName(Settings::getSetting('newsletter_plugin', 'unsubscribe_page_name', 'subscribe'));
 				if ($this->oUnsubscribePage === null) {
 					throw new Exception('Error in'.__METHOD__.': a public and hidden unsubscribe page is required for unsubscribe to function');
