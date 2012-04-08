bc1a0b946506a97bde54dc7d63b8b257b6266b6f
Jürg Messmer <jm@mosaics.ch>
2012-04-08 12:09:46 +0200

    add changes from conflict and branch mess, subscriber_import

--- lib/model/SubscriberGroupPeer.php
+++ lib/model/SubscriberGroupPeer.php
@@ -57,11 +57,35 @@ class SubscriberGroupPeer extends BaseSubscriberGroupPeer {
 		return self::doCount(new Criteria()) > 0;
 	}
 	
-	public static function getAllAssoc($bDoJoinSubscriberMemberships = false, $bAddMemberShipCount = false) {
+	/** getAllAssoc()
+	* @param boolean inner_join subscriber_group_memberships
+	* @param boolean show external and backend_created subscriber groups
+	* @param boolean show count
+	* @return array assoc for select
+	*/
+	public static function getAllAssoc($bDoJoinSubscriberMemberships=false, $bIncludeTemporaryMailGroups=true, $bAddMemberShipCount=false) {
 		$aResult = array();
 		foreach(self::getSubscriberGroups(true, $bDoJoinSubscriberMemberships) as $oSubscriberGroup) {
-		  $sAddon = $bAddMemberShipCount? ' ('.$oSubscriberGroup->countSubscriberGroupMemberships().')' : '';
-			$aResult[(string) $oSubscriberGroup->getId()] = StringPeer::getString('subscriber_group_name.'.$oSubscriberGroup->getName(), null, $oSubscriberGroup->getName()).$sAddon;
+			$iGroupId = (string) $oSubscriberGroup->getId();
+			$sOriginalName = $oSubscriberGroup->getName();
+			$iCountMemberships = '';
+			$iCountBackendCreated = '';
+			
+			// add count info if required
+			if($bAddMemberShipCount) {
+				$iCountMemberships = ' ('.$oSubscriberGroup->countSubscriberGroupMembershipsByIsBackendCreated(false).')';
+				$iCountBackendCreated = ' ('.$oSubscriberGroup->countSubscriberGroupMembershipsByIsBackendCreated(true).')';
+			}
+			
+			// display subscriber_group info depending on whether backend created subscriber_group is shown
+			if($bIncludeTemporaryMailGroups && $iCountBackendCreated !== '') {
+				$aResult[$iGroupId] = $sOriginalName." ".StringPeer::getString('wns.backend_created.original_name_suffix').$iCountMemberships;
+			} else {
+				$aResult[$iGroupId] = $sOriginalName.$iCountMemberships;
+			}
+			if($bIncludeTemporaryMailGroups && $iCountBackendCreated !== '') {
+				$aResult[$iGroupId.MailGroupInputWidgetModule::BACKEND_CREATED_SUFFIX] = $sOriginalName." ".StringPeer::getString('wns.backend_created.temporary_name_suffix').$iCountBackendCreated;
+			}
 		}
 		return $aResult;
 	}
--- modules/widget/mail_group_input/MailGroupInputWidgetModule.php
+++ modules/widget/mail_group_input/MailGroupInputWidgetModule.php
@@ -3,19 +3,31 @@
 * @package modules.widget
 */
 class MailGroupInputWidgetModule extends WidgetModule {
+
+	const BACKEND_CREATED_SUFFIX = '_import';
 	
-	public function getMailGroups() {
-		$aMailGroups = self::getSubscriberGroups();
-		// if filter is implemented in project this allows to add on-the-fly mail groups
-		FilterModule::getFilters()->handleMailGroups(array(&$aMailGroups));
+	public function getMailGroups($bIncludeTemporaryMailGroups=true) {
+		
+		//Get subscriber groups, show temporary mail groups, i.e. if not called from subscriber_import widget
+		$aMailGroups = self::getSubscriberGroups($bIncludeTemporaryMailGroups);
+		
+		//If filter is implemented in project this allows to add on-the-fly mail groups, 
+		//i.e. dynamic groups of recipients that have registered for an event
+		if($bIncludeTemporaryMailGroups) {
+			FilterModule::getFilters()->handleMailGroups(array(&$aMailGroups));
+		}
 		return $aMailGroups;
 	}
 	
-	public static function getSubscriberGroups() {
-		return SubscriberGroupPeer::getAllAssoc(false, true);
+	public static function getSubscriberGroups($bIncludeTemporaryMailGroups, $bAddMembershipCount=true) {
+		return SubscriberGroupPeer::getAllAssoc(false, $bIncludeTemporaryMailGroups, $bAddMembershipCount);
 	}
 	
 	public static function hasSubscriberGroups() {
-		return count(self::getSubscriberGroups()) > 0;
+		return count(self::getSubscriberGroups(false)) > 0;
+	}
+	
+	public function getHasBackendCreatedSubscriberGroups() {
+		return SubscriberGroupMembershipQuery::create()->filterByIsBackendCreated(true)->groupBySubscriberGroupId();->count() > 0;
 	}
 }
