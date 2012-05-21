5ff440c0aa231d9d4ecfd8035b04e521918786b7
Jürg Messmer <jm@mosaics.ch>
2012-05-21 18:27:49 +0200

    unused argument removed

--- modules/widget/mail_group_input/MailGroupInputWidgetModule.php
+++ modules/widget/mail_group_input/MailGroupInputWidgetModule.php
@@ -7,7 +7,7 @@ class MailGroupInputWidgetModule extends WidgetModule {
 	public function getMailGroups($bIncludeTemporaryMailGroups=true) {
 		
 		// Get subscriber groups, add membership count if external mailgroups are not include, i.e. called from subscriber_import widget
-		$aMailGroups = self::getSubscriberGroups($bIncludeTemporaryMailGroups);
+		$aMailGroups = self::getSubscriberGroups();
 		
 		// If filter is implemented in project this allows to add on-the-fly mail groups
 		// E.g. a group of recipients that have registered for an event and there for create a temporary mail group
@@ -17,16 +17,12 @@ class MailGroupInputWidgetModule extends WidgetModule {
 		return $aMailGroups;
 	}
 	
-	public static function getSubscriberGroups($bIncludeTemporaryMailGroups, $bAddMembershipCount=true) {
-		return SubscriberGroupPeer::getAllAssoc(false, $bIncludeTemporaryMailGroups, $bAddMembershipCount);
+	public static function getSubscriberGroups($bAddMembershipCount=true) {
+		return SubscriberGroupPeer::getAllAssoc(false, $bAddMembershipCount);
 	}
 	
 	public static function hasSubscriberGroups() {
 		return count(self::getSubscriberGroups(false)) > 0;
-	}
-	
-	public function getHasBackendCreatedSubscriberGroups() {
-		return SubscriberGroupMembershipQuery::create()->filterByIsBackendCreated(true)->groupBySubscriberGroupId()->count() > 0;
 	}
 
 }
