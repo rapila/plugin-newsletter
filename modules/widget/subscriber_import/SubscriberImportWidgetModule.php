2c2cf328f0c94fec04df7497ffab0d3af995d650
Jürg Messmer <jm@mosaics.ch>
2012-05-22 16:15:23 +0200

    generating import subscriber_group, refactor code

--- modules/widget/subscriber_import/SubscriberImportWidgetModule.php
+++ modules/widget/subscriber_import/SubscriberImportWidgetModule.php
@@ -1,6 +1,7 @@
 <?php
 class SubscriberImportWidgetModule extends PersistentWidgetModule {
 	
+	const GENERATED_PREFIX = 'Imported-';
 	public function __construct($sSessionKey = null) {
 		parent::__construct($sSessionKey);
 	}
@@ -14,8 +15,9 @@ class SubscriberImportWidgetModule extends PersistentWidgetModule {
 	*
 	* @return array of integer received all / actually added
 	*/
-	public function addSubscibers($aSubscribers, $iTargetSubscriberGroup) {
+	public function addSubscibers($aSubscribers, $mTargetSubscriberGroup) {
 		$sSubscribers = '';
+		$aTargetSubscriberGroups = is_array($mTargetSubscriberGroup) ? $mTargetSubscriberGroup : array($mTargetSubscriberGroup);
 		
 		// if is string it has not been processed and validated by js
 		if(is_string($aSubscribers)) {
@@ -30,6 +32,13 @@ class SubscriberImportWidgetModule extends PersistentWidgetModule {
 		$iCountAll = count($aSubscribers);
 		$iMembershipsAdded = 0;
 		
+		// @todo check change jm
+		// always add temporary groups with all importet subscribers
+		$oSubscriberGroup = new SubscriberGroup();
+		$oSubscriberGroup->setName(GENERATED_PREFIX.date('Y-m-d'));
+		$oSubscriberGroup->save();
+		array_push($aTargetSubscriberGroups, $oSubscriberGroup->getId());
+		
 		foreach($aSubscribers as $sEmail) {
 			$oSubscriber = SubscriberQuery::create()->filterByEmail($sEmail)->findOne();
 			
@@ -41,10 +50,18 @@ class SubscriberImportWidgetModule extends PersistentWidgetModule {
 			}
 			// add subscriber_group_membership if a subscriber exists and no subscriber_group_membership exists
 			if($oSubscriber !== null) {
-				if(!$oSubscriber->hasNewsletterBySubscriberGroupId((int) $iTargetSubscriberGroup)) {
-					$oSubscriberGroupMembership = new SubscriberGroupMembership();
-					$oSubscriberGroupMembership->setSubscriberGroupId($iTargetSubscriberGroup);
-					$oSubscriber->addSubscriberGroupMembership($oSubscriberGroupMembership);
+				$bHasMemberShip = false;
+				// @todo check change jm > handle multiple groups including generated one
+				// please check meaning and function of counting new subscriptions, only one is counted per subsriber $iMembershipsAdded
+				foreach($aTargetSubscriberGroups as $iSubscriberGroupId) {
+					if(!$oSubscriber->hasNewsletterBySubscriberGroupId((int) $iSubscriberGroupId)) {
+						$bHasMemberShip = true;
+						$oSubscriberGroupMembership = new SubscriberGroupMembership();
+						$oSubscriberGroupMembership->setSubscriberGroupId($iSubscriberGroupId);
+						$oSubscriber->addSubscriberGroupMembership($oSubscriberGroupMembership);
+					}
+				}
+				if($bHasMemberShip) {
 					$iMembershipsAdded++;
 				}
 				$oSubscriber->save();
