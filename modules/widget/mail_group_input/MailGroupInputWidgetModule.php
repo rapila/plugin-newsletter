5def8864f6eac55c09a8b4aff19021ae243138fc
Jürg Messmer <jm@mosaics.ch>
2012-05-23 17:09:11 +0200

    setting include_generated_mail_groups added

--- modules/widget/mail_group_input/MailGroupInputWidgetModule.php
+++ modules/widget/mail_group_input/MailGroupInputWidgetModule.php
@@ -4,10 +4,10 @@
 */
 class MailGroupInputWidgetModule extends WidgetModule {
 	
-	public function getMailGroups($bIncludeTemporaryMailGroups=true) {
+	public function getMailGroups($bIncludeTemporaryMailGroups=true, $bIncludeGeneratedMailGroups=true) {
 		
 		// Get subscriber groups, add membership count if external mailgroups are not include, i.e. called from subscriber_import widget
-		$aMailGroups = self::getSubscriberGroups();
+		$aMailGroups = self::getSubscriberGroups(true, $bIncludeGeneratedMailGroups);
 		
 		// If filter is implemented in project this allows to add on-the-fly mail groups
 		// E.g. a group of recipients that have registered for an event and there for create a temporary mail group
@@ -17,8 +17,8 @@ class MailGroupInputWidgetModule extends WidgetModule {
 		return $aMailGroups;
 	}
 	
-	public static function getSubscriberGroups($bAddMembershipCount=true) {
-		return SubscriberGroupPeer::getAllAssoc(false, $bAddMembershipCount);
+	public static function getSubscriberGroups($bAddMembershipCount=true, $bIncludeGeneratedMailGroups=true) {
+		return SubscriberGroupPeer::getAllAssoc(false, $bAddMembershipCount, $bIncludeGeneratedMailGroups);
 	}
 	
 	public static function hasSubscriberGroups() {
