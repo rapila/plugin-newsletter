71726ae3a5671af0d904cc2014752bdabaa2587d
Jürg Messmer <jm@mosaics.ch>
2012-08-16 10:07:21 +0200

    refactored peer methods to use local query call

--- modules/widget/subscriber_group_input/SubscriberGroupInputWidgetModule.php
+++ modules/widget/subscriber_group_input/SubscriberGroupInputWidgetModule.php
@@ -6,9 +6,7 @@
 class SubscriberGroupInputWidgetModule extends WidgetModule {
 	
 	public function getSubscriberGroups() {
-		$bGetGroupsWithSubscribersOnly = false;
-		$aSubscriberGroups = SubscriberGroupPeer::getSubscriberGroups(true, $bGetGroupsWithSubscribersOnly);
-		$aSubscriberGroups = WidgetJsonFileModule::jsonBaseObjects($aSubscriberGroups, array('id', 'name'));
+		$aSubscriberGroups = SubscriberGroupQuery::create()->orderByName()->select(array('Id', 'Name'))->find()->toKeyValue('Id', 'Name');
 		// @todo: handle external mail groups
 		// FilterModule::getFilters()->handleMailGroups(&$aSubscriberGroups));
 		return $aSubscriberGroups;
